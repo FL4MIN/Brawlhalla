02f8e2c2c2b2bad4f2b532ae4b26db079743960a
FL4MIN <32968126+FL4MIN@users.noreply.github.com>
2023-08-24 05:38:37 +0200

    Update cerca.js

--- cerca.js
+++ cerca.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('#basket,#account').hover(function() {
-    $('#currency').css('width', '70px');
+    $('#currency').css('width', '160px');
   }, function() {
     // on mouseout, reset the background colour
     $('#currency').css('width', '');
