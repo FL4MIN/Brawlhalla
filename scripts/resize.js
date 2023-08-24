f712ccb0a2409f1357ce6f94b15096d25d932d98
FL4MIN <32968126+FL4MIN@users.noreply.github.com>
2023-08-24 05:58:50 +0200

    Update resize.js

--- scripts/resize.js
+++ scripts/resize.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('#basket,#account').hover(function() {
-    $('#currency').css('width', '350px');
+    $('#currency').css('width', '100px');
   }, function() {
     // on mouseout, reset the background colour
     $('#currency').css('width', '');
